5a3f6fcb7f762cd902405ba32afea80035b9c43f
Shivam <mishrashivam2027@gmail.com>
2026-08-19 12:05:16 +0530

    Allow production frontend CORS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173"
+    origin: [
+      "http://localhost:5173",
+      "https://nexusgate-frontend.onrender.com"
+    ]
   })
 );
 
